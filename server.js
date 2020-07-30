018d919b1e6c5b238b1b107bd45254ede517e512
Jermaine <jermerf@gmail.com>
2020-07-30 10:53:36 -0300

    Comment

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(notes)
 app.use(users)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('dist'))
+  app.use(express.static('dist')) // Where compiled files go for a vue project
+  // app.use(express.static('public')) // Where static web files go in a generic (non-vue) project
   console.log("Serving dist folder")
 } else {
   console.log("Dev mode, only serving API")
